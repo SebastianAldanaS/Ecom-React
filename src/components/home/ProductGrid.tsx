f437531ac3f1892b946525a2b5ab4c65e96737e5
Sebastian Aldana S. <sebas.a.s1313@hotmail.com>
2024-11-26 03:22:10 -0500

    Escaparate de celulares

--- src/components/home/ProductGrid.tsx
+++ src/components/home/ProductGrid.tsx
@@ -1,6 +1,9 @@
+import { CardProduct } from "../products/CardProduct";
+import { prepareProducts } from "../../interfaces";
+
 interface Props {
     title: string;
-    products: any[];
+    products: prepareProducts[];
 }
 
 export const ProductGrid = ({title, products}: Props) => {
@@ -10,9 +13,14 @@ export const ProductGrid = ({title, products}: Props) => {
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8">
             {products.map((product) => (
-                <div className="flex-col gap-6 relative" key={product.id}>
-                    <h3>{product.title}</h3>
-                </div> 
+                <CardProduct 
+                    key={product.id} 
+                    name={product.name} 
+                    price={product.price} 
+                    colors={product.colors} 
+                    img={product.images[0]} 
+                    slug={product.slug} 
+                    variants={product.variants}  />
             ))}
         </div>
     </div>
--- src/components/products/CardProduct.tsx
+++ src/components/products/CardProduct.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
 import { useState } from "react";
+import { VariantProduct } from "../../interfaces";
+import { formatPrice } from "../../helpers";
 
 interface Props {
     img: string;
@@ -8,33 +10,38 @@ interface Props {
     price: number;
     slug: string;
     colors: {name: string, color: string} [];
-    variants: any[];
+    variants: VariantProduct[];
 }
 
 export const CardProduct = (
     {img, name, price, slug, colors, variants}: Props
 ) => {
 
-    const [ActiveColor, setActiveColor] = useState
+    const [activeColor, setActiveColor] = useState
     <{name: string, color: string}>(colors[0]);
+
+    const selectdVariant = variants.find(variant => variant.color === activeColor.color);
+
+    const stock = selectdVariant.stock || 0;
+
+    
     
     return (
         <div className="flex flex-col gap-6 relative">
-            <Link to={ `/celulares/${slug}` } className="flex relative group">
+            <Link to={ `/celulares/${slug}` } className="flex relative group overflow-hidden">
                 <div className="flex h-[350px] w-full items-center justify-center py-2 lg:h-[250px]">
                     <img src={img} alt={name} 
                     className='object-contain h-full w-full'/>
                 </div>
-                <button className="bg-white border border-slate-200 absolute w-full bottom-0 py-5 rounded-3x1 flex items-center justify-center gap-1 text-sm font-medium hover:bg-stone-100 translate-y-[100%] transition-all duration-300 group-hover:translate-y-0">
-                    <FiPlus>
-                        Añadir al carrito
-                    </FiPlus>
+                <button className="bg-white border border-slate-200 absolute w-full bottom-0 py-3 rounded-3xl flex items-center justify-center gap-1 text-sm font-medium hover:bg-stone-100 translate-y-[100%] transition-all duration-300 group-hover:translate-y-0">
+                <FiPlus />
+                Añadir
                 </button>
             </Link>
 
             <div className="flex flex-col gap-1 items-center">
                 <p className='text-[15px] font-medium'>{name}</p>
-                <p className='text-[15px] font-medium'>{price}</p>
+                <p className='text-[15px] font-medium'>{formatPrice(price)}</p>
                 <div className="flex gap-3">
                     {colors.map(color => (
                         <span key={color.color}
